9bcd6e83be7286c78ab0e2c3b9683ea6e4d51f2f
Menza <menzamenza90@gmail.com>
2024-05-02 14:22:06 +0000

    bloquer après avoir épusé le nbre d'essai

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,3 @@
-
-
 // generer un nombre aléatoire
 const min = 10;
 const max = 100;
@@ -7,20 +5,27 @@ const random = Math.floor(Math.random() * (max - min)) + min;
 console.log(random);
 let essaiMax = 10;
 
-document.getElementById('nbreEssai').innerText =essaiMax
+document.getElementById('nbreEssai').innerText = essaiMax;
+
 function decremente() {
     essaiMax -= 1;
+    document.getElementById('nbreEssai').innerText = essaiMax;
+    if (essaiMax === 0) {
+        document.getElementById('envoyer').disabled = true;
+        document.getElementById('result').innerText = "Le nombre d'essai est épuisé MDR 😂";
+
+    }
 }
 
 // ecouteur d'events sur la soumission du formulaire
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    let nbreMis = document.getElementById('nbreEntre').value;
+    let nbreMis = parseInt(document.getElementById('nbreEntre').value);
     console.log(nbreMis);
 
     for (let essai = 1; essai <= essaiMax; essai++) { // Utiliser essaiMax comme condition de la boucle
-        if (nbreMis == random) {
+        if (nbreMis === random) {
             document.getElementById('result').innerText = "BRAVOOOOOOOO! Vous avez bien deviné ❤️ ";
             break;
         } else if (nbreMis < random) {
@@ -28,11 +33,9 @@ document.querySelector('form').addEventListener('submit', function (e) {
             console.log(essaiMax);
         } else if (nbreMis > random) {
             document.getElementById('result').innerText = "Masta, le nombre est plus petit mdrr!🔽 ";
-        } else {
+        }else {
             document.getElementById('result').innerText = "Veuillez entrer un nombre valide";
         }
     }
     decremente();
-    document.getElementById('nbreEssai').innerText =essaiMax
 });
-
