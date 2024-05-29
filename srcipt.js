5312ceededf9700a52834f3ab28ac182eaed969c
Menza <menzamenza90@gmail.com>
2024-05-29 14:29:08 +0000

    DISABLED quand u gagnes

--- srcipt.js
+++ srcipt.js
@@ -13,20 +13,21 @@ function decremente() {
     if (essaiMax === 0) {
         document.getElementById('envoyer').disabled = true;
         document.getElementById('result').innerText = "Le nombre d'essai est épuisé MDR 😂";
-
+        
     }
 }
 
 // ecouteur d'events sur la soumission du formulaire
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
-
+    
     let nbreMis = parseInt(document.getElementById('nbreEntre').value);
     console.log(nbreMis);
-
+    
     for (let essai = 1; essai <= essaiMax; essai++) { // Utiliser essaiMax comme condition de la boucle
         if (nbreMis === random) {
             document.getElementById('result').innerText = "BRAVOOOOOOOO! Vous avez bien deviné ❤️ ";
+            document.getElementById('envoyer').disabled = true;
             break;
         } else if (nbreMis < random) {
             document.getElementById('result').innerText = "Non wsh, le nombre est plus grand 🔼";
